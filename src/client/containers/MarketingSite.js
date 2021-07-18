f6285188f9a6520dba075e65c3f086ee9a5e548e
Chris Ramesh <chris.ramesh@gmail.com>
2021-07-17 23:01:28 -0700

    marketing page fixes

--- src/client/containers/MarketingSite.js
+++ src/client/containers/MarketingSite.js
@@ -5,11 +5,10 @@ const MarketingSite = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <a
-        type="button"
+        className="inline-flex items-center px-6 py-3 border border-transparent shadow-sm rounded-md bg-indigo-600 hover:bg-indigo-700 text-base text-white font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         href={`${apiURL}/api/auth/discord`}
-        className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
-        {"Add Jank Poll to your Discord Server!"}
+        Add Jank Poll to your Discord Server!
       </a>
     </div>
   );
